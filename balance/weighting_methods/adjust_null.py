cd3f34fa2786e72c512b082d408fb010609ced0d
Tal Galili <talgalili@meta.com>
2022-11-16 12:49:25 -0800

    Fix docs: "adjust_null.py.py"
    Summary: NA
    
    Reviewed By: SarigT
    
    Differential Revision: D41350710
    
    fbshipit-source-id: 27d41b0d8e09651beecf72dc8f3d70c6c62c49ca

--- balance/weighting_methods/adjust_null.py
+++ balance/weighting_methods/adjust_null.py
@@ -6,28 +6,35 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
-from typing import Dict
+from typing import Dict, Union
+
+import pandas as pd
 
 logger: logging.Logger = logging.getLogger(__package__)
 
 
 def adjust_null(
-    sample_df, sample_weights, target_df, target_weights, *args, **kwargs
-) -> Dict[str, Dict[str, str]]:
-    """
-    Doesn't apply any adjustment to the data. Returns the design weights as they are.
+    sample_df: pd.DataFrame,
+    sample_weights: pd.Series,
+    target_df: pd.DataFrame,
+    target_weights: pd.Series,
+    *args,
+    **kwargs,
+) -> Dict[str, Union[Dict[str, str], pd.Series]]:
+    """Doesn't apply any adjustment to the data. Returns the design weights as they are.
     This may be useful when one needs the output of Sample.adjust() (i.e.: an adjusted object),
     but wishes to not run any model for it.
 
-    Arguments:
-        sample_df --- (pandas dataframe) a dataframe representing the sample
-        sample_weights --- (pandas series) design weights for sample
-        target_df ---  (pandas dataframe) a dataframe representing the target
-        target_weights --- (pandas series) design weights for target
+    Args:
+        sample_df (pd.DataFrame): a dataframe representing the sample
+        sample_weights (pd.Series): design weights for sample
+        target_df (pd.DataFrame): a dataframe representing the target
+        target_weights (pd.Series): design weights for target
 
     Returns:
-        Dict of weights (original sample weights) and model (with method = null_adjustment)
+        Dict[str, Union[Dict[str, str], pd.Series]]: Dict of weights (original sample weights) and model (with method = null_adjustment)
     """
+
     return {
         "weights": sample_weights,
         "model": {
