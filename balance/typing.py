0bfc653467e2320ac169d72226b703f3f4d1b4c6
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2024-02-26 14:15:59 -0800

    Pyre Configurationless migration for] core_stats
    Reviewed By: connernilsen
    
    Differential Revision: D54200147
    
    fbshipit-source-id: e8874d1597b6155b87a2acfb982dc3500eb8bff5

--- balance/typing.py
+++ balance/typing.py
@@ -3,6 +3,8 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
+# pyre-unsafe
+
 # LICENSE file in the root directory of this source tree.
 
 from pathlib import Path
