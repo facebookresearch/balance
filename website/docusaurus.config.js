c5f0b0a71a4c8e43899b9be70f5816450a828c08
Tal Galili <talgalili@meta.com>
2025-10-28 08:03:47 -0700

    Add changelog to Balance website top navigation (#131)
    Summary:
    
    This change adds the CHANGELOG to the Balance website's top navigation bar, making it easier for users to track project updates and version history directly from the website.
    
    Differential Revision: D85657589

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,8 +1,8 @@
 /**
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
- * This software may be used and distributed according to the terms of the
- * GNU General Public License version 2.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
@@ -91,6 +91,12 @@ const katex = require('rehype-katex');
             docId: 'api_reference/index',
             position: 'right',
             label: 'API Reference',
+          },
+          {
+            type: 'doc',
+            docId: 'docs/changelog',
+            position: 'right',
+            label: 'Changelog',
           },
           // Please keep GitHub link to the right for consistency.
           {
