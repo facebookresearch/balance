f19b1e3df90422fc2da635651ac342d3116bbbb9
Soumyadip Sarkar <soumya.papanvk18@gmail.com>
2026-01-22 23:56:45 +0530

    Update assertion

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -124,7 +124,6 @@ class TestDatasets(
 
         # Check column types for both dataframes
         for df in [target_df, sample_df]:
-            self.assertEqual(df["id"].dtype, object)  # String type
             self.assertTrue(
                 df["gender"].dtype == object
                 or pd.api.types.is_string_dtype(df["gender"])
